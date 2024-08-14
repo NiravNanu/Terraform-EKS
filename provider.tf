8ee23faf0c2eb31bc01c73d504c2117d250ff70f
Nirav <Nirav9019@yahoo.com>
2024-08-14 14:47:08 -0400

    Files added

---
+++ provider.tf
@@ -0,0 +1,16 @@
+#https://registry.terraform.io/providers/hashicorp/aws/latest/docs
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
+#configure the AWS Provider
+
+provider "aws" {
+  region = var.aws_region
+}
